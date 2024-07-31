5d7cf13dd36d77e709551cb1ef3142447ca6fb48
eddfulus <fareedsaroni@gmail.com>
2024-07-31 14:58:22 +0800

    Add a new page for submitting a listing

---
+++ app/submit-listing/page.tsx
@@ -0,0 +1,8 @@
+export default function SubmitListing() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4">Submit a Listing</h1>
+      <p>Form for submitting a new listing will be added here.</p>
+    </div>
+  );
+}
