b7e2881e0567de405b5ffcbbafb789901bb63774
eddfulus <fareedsaroni@gmail.com>
2024-07-31 15:55:26 +0800

    Add a new Listings page component to display vacation rentals in Malaysia.

---
+++ app/listings/page.tsx
@@ -0,0 +1,8 @@
+export default function Listings() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4">Vacation Rentals in Malaysia</h1>
+      <p>List of vacation rentals will be displayed here.</p>
+    </div>
+  );
+}
